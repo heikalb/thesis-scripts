39e0f2bbd30d91c73c8bbd3d94004a96e26e6e88
hbadrulh <hbadrulh@sfu.ca>
2019-02-28 11:15:46 -0800

    Completed last method

--- preprocess/util.py
+++ preprocess/util.py
@@ -1,14 +1,16 @@
 """
-Apply spell spell correction suggestions to matching words
+Functions for preprocessing queries
 Heikal Badrulhisham <heikal93@gmail.com>, 2019
 """
 spell_corrections = open('verb_spelling_suggestions_2.txt').read().split('\n')
 spell_corrections = [line.split() for line in spell_corrections]
 spell_corrections = [l for l in spell_corrections if not (l[0] == '#' or l[0] == '@' or len(l) > 2)]
-
 sentence_punct = ['.', '!', '?']
 
 
+"""
+Apply spelling correction based on spell correction suggestion file
+"""
 def apply_correction(target_word):
     target_word_normalized = ''.join([ch for ch in target_word if ch.isalpha()])
     target_word_normalized = target_word_normalized.lower()
@@ -21,6 +23,9 @@ def apply_correction(target_word):
     return target_word
 
 
+"""
+Helper method for apply_correction(). Restores punctuation back into a word
+"""
 def restore_punct(word_1, word_2):
     if not any(not ch.isalpha() for ch in word_1):
         return word_2
@@ -42,10 +47,11 @@ def restore_punct(word_1, word_2):
     return '{0}{1}{2}'.format(left_punct, word_2, right_punct)
 
 
-def to_one_sentence(sentence, target_word):
+"""
+Given a sentence that may span multiple sentence boundaries, return the sentence containing the target word only
+"""
+def to_one_sentence(sentence, target_i):
     tokens = sentence.split()
-    target_i = tokens.index(target_word)
-    new_sent_words = [t for t in tokens]
     right_sent = []
     left_sent = []
 
@@ -55,10 +61,15 @@ def to_one_sentence(sentence, target_word):
         if any(punct in t for punct in sentence_punct):
             break
 
-    tokens_ = [t for t in tokens]
+    i = target_i - 1
 
+    while i >= 0:
+        if any(punct in tokens[i] for punct in sentence_punct):
+            break
 
+        left_sent.insert(0, tokens[i])
 
+        i -= 1
 
-    return ' '.join(new_sent_words)
+    return ' '.join(left_sent + right_sent)
 
