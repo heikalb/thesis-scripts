acc01113caf55b646e0b2faff1a214c5dac80463
hbadrulh <hbadrulh@sfu.ca>
2019-03-01 10:12:46 -0800

    meep

--- preprocess/collect_queries.py
+++ preprocess/collect_queries.py
@@ -6,7 +6,7 @@ Heikal Badrulhisham <heikal93@gmail.com>, 2019
 import csv
 import sys
 sys.path.append('../../')
-import preprocess.util as spell_correction
+import preprocess.util as util
 
 
 def main():
@@ -22,12 +22,13 @@ def main():
                     first_row = False
                     continue
 
-                main_word = spell_correction.apply_correction(row[3])
-                full_sentence = ' '.join([row[2], main_word, row[4]])
+                main_word = util.apply_correction(row[3])
                 before_context_size = len(row[2].split(' '))
+                full_sentence = ' '.join([row[2], main_word, row[4]])
+                full_sentence = util.to_one_sentence(full_sentence, before_context_size)
                 rows.append([full_sentence, main_word, before_context_size])
 
-    with open('../data/query_results_all_joined_sents.tsv', 'w') as f:
+    with open('../data/query_results_all_joined_sents_.tsv', 'w') as f:
         csv_writer = csv.writer(f, delimiter='\t')
 
         for r in rows:
