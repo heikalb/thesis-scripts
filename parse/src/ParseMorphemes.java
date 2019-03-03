88e12fe8c4eea32a8a5ab94611b5ef225301d550
hbadrulh <hbadrulh@sfu.ca>
2019-03-03 12:06:24 -0800

    meep

--- parse/src/ParseMorphemes.java
+++ parse/src/ParseMorphemes.java
@@ -1,15 +1,12 @@
 import java.util.List;
 import zemberek.morphology.TurkishMorphology;
-import zemberek.core.logging.Log;
 import zemberek.morphology.analysis.SentenceAnalysis;
-import zemberek.morphology.analysis.SingleAnalysis;
 import zemberek.morphology.analysis.WordAnalysis;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 import java.io.FileWriter;
 import java.io.File;
 
@@ -27,18 +24,14 @@ public class ParseMorphemes
         //Collect parses
         ArrayList<List> parses = new ArrayList();
 
-        int i = 0;
-
         // Go through lines in file
         String line;
-        while ((line = br.readLine()) != null && i < 5)
+        while ((line = br.readLine()) != null)
         {
             String sentence = line.split("\t")[0];
             List<WordAnalysis> analyses = morphology.analyzeSentence(sentence);
             SentenceAnalysis result = morphology.disambiguate(sentence, analyses);
             parses.add(result.bestAnalysis());
-
-            i++;
         }
 
         // Save data
@@ -47,9 +40,7 @@ public class ParseMorphemes
 
 
         for(List parseList: parses)
-        {
             writer.append(parseList.toString() + '\n');
-        }
 
         writer.flush();
         writer.close();
