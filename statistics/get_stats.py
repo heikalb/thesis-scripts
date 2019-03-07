423bf2eaa8181da1c101c821913a46507de96060
hbadrulh <hbadrulh@sfu.ca>
2019-03-07 14:22:26 -0800

    meep

--- statistics/get_stats.py
+++ statistics/get_stats.py
@@ -4,7 +4,7 @@ import re
 import colloc_measures as cm
 
 
-def main(right_parse_sign, suffix_boundary, mboundary, key_separator):
+def colloc_stats(right_parse_sign, suffix_boundary, mboundary, key_separator, query_term=""):
     # Open file of parses
     parses = open('../parse/parses_verbs.txt', 'r').read().split('\n')
     parses = [p.split() for p in parses]
@@ -20,8 +20,8 @@ def main(right_parse_sign, suffix_boundary, mboundary, key_separator):
 
     # Go through parses
     for parse in parses:
-        # Skip wrong parses
-        if right_parse_sign not in parse[0]:
+        # Skip parses for a different stem or wrong parses
+        if query_term not in parse[0] or right_parse_sign not in parse[0]:
             continue
 
         # Get suffixes, exclude stems
@@ -59,43 +59,51 @@ def main(right_parse_sign, suffix_boundary, mboundary, key_separator):
                           suff_freq[m1], suff_freq[m2])
 
     # Save data
-    with open('cooccurrence_count.csv', 'w') as f:
+    with open('cooccurrence_count/cooccurrence_count_{0}.csv'.format(query_term), 'w') as f:
         csv_writer = csv.writer(f)
 
         for k in cooc_freq:
             csv_writer.writerow([k, cooc_freq[k]])
 
-    with open('suffix_count.csv', 'w') as f:
+    with open('suffix_count/suffix_count_{0}.csv'.format(query_term), 'w') as f:
         csv_writer = csv.writer(f)
 
         for k in suff_freq:
             csv_writer.writerow([k, suff_freq[k]])
 
-    with open('mutual_info/mutual_info.csv', 'w') as f:
+    with open('mutual_info/mutual_info_{0}.csv'.format(query_term), 'w') as f:
         csv_writer = csv.writer(f)
 
         for k in mutual_infos:
             csv_writer.writerow([k, mutual_infos[k][0], mutual_infos[k][1], mutual_infos[k][2]])
 
-    with open('t_scores/t_scores.csv', 'w') as f:
+    with open('t_scores/t_scores_{0}.csv'.format(query_term), 'w') as f:
         csv_writer = csv.writer(f)
 
         for k in t_scores:
             csv_writer.writerow([k, t_scores[k][0], t_scores[k][1], t_scores[k][2]])
 
-    with open('dice_coeff/dice_coeff.csv', 'w') as f:
+    with open('dice_coeff/dice_coeff_{0}.csv'.format(query_term), 'w') as f:
         csv_writer = csv.writer(f)
 
         for k in dice_coeff:
             csv_writer.writerow([k, dice_coeff[k][0], dice_coeff[k][1], dice_coeff[k][2]])
 
-    with open('chi_squared/chi_squared.csv', 'w') as f:
+    with open('chi_squared/chi_squared_{0}.csv'.format(query_term), 'w') as f:
         csv_writer = csv.writer(f)
 
         for k in chi_squared:
             csv_writer.writerow([k, chi_squared[k][0], chi_squared[k][1], chi_squared[k][2]])
 
 
+def main():
+    query_terms = [""] + open('../data/query_terms.txt', 'r').read().split('\n')
+
+    for qt in query_terms:
+        colloc_stats(right_parse_sign='Verb', suffix_boundary=r'[\|\+]', mboundary=r'.*:',
+                     key_separator='__', query_term=qt)
+
+
 if __name__ == "__main__":
-    main(right_parse_sign='Verb', suffix_boundary=r'[\|\+]', mboundary=r'.*:', key_separator='__')
+    main()
     exit(0)
