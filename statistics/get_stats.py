4b66a9d6cb5ccc167b6ad4a5b7832e7500962ef4
hbadrulh <hbadrulh@sfu.ca>
2019-03-14 12:44:50 -0700

    progress

--- statistics/get_stats.py
+++ statistics/get_stats.py
@@ -2,16 +2,11 @@
 Get various collocation statistics from the corpus
 Heikal Badrulhisham <heikal93@gmail.com>, 2019
 """
-
 import csv
 from collections import defaultdict
 import re
 import colloc_measures as cm
 import os
-
-measures = ['mutual_information', 't_score', 'dice_coefficient', 'chi_squared']
-measure_funct = dict(zip(measures, [cm.mutual_info, cm.t_score, cm.dice_coeff, cm.chi_squared]))
-measure_dict = dict(zip(measures, [dict() for m in measures]))
 
 
 def new_dir(dir_name):
@@ -20,17 +15,19 @@ def new_dir(dir_name):
 
 
 def colloc_stats(right_parse_sign, suffix_boundary, mboundary, key_separator, query_term=""):
+    # Statistics
+    measures = ['mutual_information', 't_score', 'dice_coefficient', 'chi_squared']
+    measure_funct = dict(zip(measures, [cm.mutual_info, cm.t_score, cm.dice_coeff, cm.chi_squared]))
+    measure_dict = dict(zip(measures, [dict() for m in measures]))
+    abs_msr = {'suff_freq': defaultdict(int), 'cooc_freq': defaultdict(int)}
+
     # Open file of parses
     parses = open('../parse/parses_verbs.txt', 'r').read().split('\n')
     parses = [p.split() for p in parses]
 
-    # Stats
-    suff_freq = defaultdict(int)
-    cooc_freq = defaultdict(int)
-
     # Go through parses
     for parse in parses:
-        # Skip parses for a different stem or wrong parses
+        # Skip parses for a different stem or wrong parses (parse[0] is the dictionary form of the verb)
         if query_term not in parse[0] or right_parse_sign not in parse[0]:
             continue
 
@@ -43,37 +40,32 @@ def colloc_stats(right_parse_sign, suffix_boundary, mboundary, key_separator, qu
         # Count suffix (co)occurrences
         for i in range(len(suffixes)):
             # Updata single suffix count
-            suff_freq[suffixes[i]] += 1
+            abs_msr['suff_freq'][suffixes[i]] += 1
 
             # Update count for co-occurring pairs
             for j in range(i + 1, len(suffixes)):
                 curr_key = '{0}{1}{2}'.format(suffixes[i], key_separator, suffixes[j])
-                cooc_freq[curr_key] += 1
+                abs_msr['cooc_freq'][curr_key] += 1
 
-    num_suffixes = sum(suff_freq[s] for s in suff_freq)
+    num_suffixes = sum(abs_msr['suff_freq'][s] for s in abs_msr['suff_freq'])
 
     # Get association measures
-    for k in cooc_freq:
+    for k in abs_msr['cooc_freq']:
         m1, m2 = k.split(key_separator)
 
         for msr in measures:
-            args = [suff_freq[m1], suff_freq[m2], cooc_freq[k], num_suffixes, m1, m2, cooc_freq]
-            measure_dict[msr][k] = (measure_funct[msr](*args), suff_freq[m1], suff_freq[m2])
+            args = [abs_msr['suff_freq'][m1], abs_msr['suff_freq'][m2], abs_msr['cooc_freq'][k],
+                    num_suffixes, m1, m2, abs_msr['cooc_freq']]
+            measure_dict[msr][k] = (measure_funct[msr](*args), abs_msr['suff_freq'][m1], abs_msr['suff_freq'][m2])
 
     # Save data
-    new_dir('cooccurrence_count')
-    with open('cooccurrence_count/cooccurrence_count_{0}.csv'.format(query_term), 'w') as f:
-        csv_writer = csv.writer(f)
+    for m in abs_msr:
+        new_dir(m)
+        with open('{0}/{0}_{1}.csv'.format(m, query_term), 'w') as f:
+            csv_writer = csv.writer(f)
 
-        for k in cooc_freq:
-            csv_writer.writerow([k, cooc_freq[k]])
-
-    new_dir('suffix_count')
-    with open('suffix_count/suffix_count_{0}.csv'.format(query_term), 'w') as f:
-        csv_writer = csv.writer(f)
-
-        for k in suff_freq:
-            csv_writer.writerow([k, suff_freq[k]])
+            for k in abs_msr[m]:
+                csv_writer.writerow([k, abs_msr[m][k]])
 
     for msr in measures:
         new_dir(msr)
