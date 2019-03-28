79c1ffc983a41e045dbc75bba458ebbd094c8200
hbadrulh <hbadrulh@sfu.ca>
2019-03-28 00:07:16 -0700

    restructure

--- 3_preprocess_data/add_spell_suggestion.py
+++ 3_preprocess_data/add_spell_suggestion.py
@@ -7,36 +7,61 @@ Heikal Badrulhisham, 2019
 import re
 # Spelling substitutions
 suggestions = {
+    r'uo([mdl].+)?$':              'uyor',
+    r'ıo([mdl].+)?$':              'ıyor',
+    r'io([mdl].+)?$':              'iyor',
+    r'üo([mdl].+)?$':              'üyor',
+    r'iyo([mdl].+)?$':            'iyor',
+    r'ıyo([mdl].+)?$':            'ıyor',
+    r'uyo([mdl].+)?$':            'uyor',
+    r'üyo([mdl].+)?$':            'üyor',
+
     r'iom$':            'iyorum',
-    r'ıom$':            'iyorum',
-    r'uo':              'uyor',
-    r'ıo':              'ıyor',
-    r'io':              'iyor',
-    r'iyo$':            'iyor',
-    r'[uü]yo$':         'uyor',
+    r'ıom$':            'ıyorum',
     r'iyom$':           'iyorum',
+    r'ıyom$':           'ıyorum',
     r'uyom$':           'uyorum',
+    r'üyom$':           'üyorum',
+
     r'iyon$':           'iyorsun',
     r'uyon$':           'uyorsun',
+    r'ıyon$':           'ıyorsun',
+    r'üyon$':           'üyorsun',
+
     r'iyoz$':           'iyoruz',
     r'ıyoz$':           'ıyoruz',
     r'uyoz$':           'uyoruz',
+    r'üyoz$':           'üyoruz',
+
     r'micek':           'meyecek',
-    r'micez':           'meyeceğiz',
+    r'mıcek':           'mayacak',
     r'[uı]cak':         'acak',
     r'icek':            'ecek',
     r'iycek':           'eyecek',
     r'ıycak':           'ayacak',
+
     r'[ıau]caz$':       'acağız',
     r'[ie]cez$':        'eceğiz',
+    r'micez$':          'meyeceğiz',
+    r'mıcaz$':          'mayacağız',
+    r'iycez$':          'eyeceğiz',
+    r'ıycaz$':          'ayacağız',
+
     r'[au]cam$':        'acağım',
     r'[ie]cem$':        'eceğim',
-    r'mıcam':           'mayacağım',
-    r'micem':           'meyeceğim',
+    r'mıcam$':          'mayacağım',
+    r'micem$':          'meyeceğim',
+    r'iycem':           'eyecem',
+    r'ıycam':           'ayacam',
+
     r'e+cen$':          'eceksin',
     r'[au]+can$':       'acaksın',
-    r'mıycak':          'mayacak',
-    r'miycek':          'meyecek',
+
+    r'canı$':           'cağını',
+    r'ceni$':           'ceğini',
+
+    r'ıyan':           'ayan',
+
 }
 
 
