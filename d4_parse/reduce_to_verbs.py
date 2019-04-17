e841675a33800011bf16ece967e001a4acf90559
hbadrulh <heikal93@gmail.com>
2019-04-17 00:00:48 -0700

    save register data

--- d4_parse/reduce_to_verbs.py
+++ d4_parse/reduce_to_verbs.py
@@ -14,6 +14,14 @@ def get_verb_indices(fpath):
     return indices
 
 
+def get_register(fpath):
+    with open(fpath) as f:
+        reader = csv.reader(f, delimiter='\t')
+        indices = [row[-1] for row in reader]
+
+    return indices
+
+
 def main():
     # Get individual verb parse files
     parse_dir_path = 'parses/'
@@ -30,17 +38,18 @@ def main():
         fnum = fname.split('_')[0]
         stem = fname.split('_')[1]
         indices = get_verb_indices('../d2_data/joined/{0}_{1}_joined.tsv'.format(fnum, stem))
+        registers = get_register('../d2_data/joined/{0}_{1}_joined.tsv'.format(fnum, stem))
 
         # Get target verbs and parses with errors
         for i in range(len(indices)):
             crnt_parses = [p for p in parses[i][1:-1].split(', ') if ':Punc' not in p]
-            verb_parses.append(crnt_parses[indices[i]])
+            verb_parses.append('{0} {1}'.format(crnt_parses[indices[i]], registers[i]))
 
             if 'UNK' in crnt_parses[indices[i]]:
                 parse_errors.append(crnt_parses[indices[i]])
 
     # Save data
-    with open('verbs_parses.txt', 'w') as f:
+    with open('verb_parses.txt', 'w') as f:
         f.write('\n'.join(verb_parses))
 
     with open('parse_errors.txt', 'w') as f:
