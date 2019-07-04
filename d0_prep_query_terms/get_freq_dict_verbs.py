712ceb938aa3774392281cee61225bd1b90117fe
hbadrulh <heikal93@gmail.com>
2019-07-03 17:29:32 -0700

    prettification

--- d0_prep_query_terms/get_freq_dict_verbs.py
+++ d0_prep_query_terms/get_freq_dict_verbs.py
@@ -1,60 +1,87 @@
 """
-Extract highest frequency verb stems from the text file of the Frequency Dictionary of Turkish.
+Extract highest frequency verb stems from the txt file of the Frequency Dictionary of Turkish.
 Heikal Badrulhisham <heikal93gmail.com>, 2019
 """
 import re
 
 
-def main():
+# Get pairs of frequency indices and verb stems from the Frequency Dictionary
+def get_indices_stems(freq_dict):
     # Section of the dictionary with the verb stems
-    verb_section = open('freq_dict.txt', 'r').read().split('\n')[75067:77349]
+    verb_section = freq_dict.read().split('\n')[75067:77349]
     verb_section = '\n'.join([line for line in verb_section if line])
-    # Isolate verb stems
-    verb_section = re.findall(r'\d+\s*\|\s*\d+\n*.+\sto.*', verb_section)
-    verb_section = [line.replace('\n', ' ') for line in verb_section if line.split()[4] == 'to']
-    stems = []
 
-    # Get index number from dictionary for sorting the stems
-    for line in verb_section:
+    # Isolate verb stems
+    verb_lines = re.findall(r'\d+\s*\|\s*\d+\n*.+\sto.*', verb_section)
+    verb_lines = [line.replace('\n', ' ') for line in verb_lines if line.split()[4] == 'to']
+
+    # Pairs of frequency indices and stems
+    indices_stems = []
+
+    # Get frequency indicesfrom the dictionary for sorting the stems
+    for line in verb_lines:
         index = int(line.split('|')[0].strip())
         stem_line = ' '.join(line.split()[3:])
-        stems.append((index, stem_line))
+        indices_stems.append((index, stem_line))
+
+    return indices_stems
+
+
+# Apply spelling and morphological corrections to stems
+def stem_correction(stem_n_index, h_words, h_corrections, morph_corrections):
+    stem = stem_n_index[1].split()[0]
+    stem = stem.replace('j', 'ğ')
+
+    if stem not in h_words:
+        stem = stem.replace('h', 'ş')
+    elif stem in h_correction:
+        stem = h_correction[stem]
+
+    if stem in morph_correction:
+        stem = morph_correction[stem]
+
+    return stem
+
+
+def main():
+    # Open Frequency Disctionary file
+    freq_dict = open('freq_dict.txt', 'r')
+
+    # Get pairs of frequency indices and verb stems
+    indices_stems = get_indices_stems(freq_dict)
 
     # Account for error in txt file of the dictionary
-    stems.append((699, 'yapılaş to strcuture'))
-    stems.sort()
+    indices_stems.append((699, 'yapılaş to strcuture'))
 
-    # Spelling and morphological correction
-    h_words = ['hazırla', 'hisset', 'hesapla', 'bahset', 'harca', 'hızlan', 'hedefle', 'rahatla', 'hohlan', 'hallet',
-               'zehirle', 'haykır', 'heyecan', 'hükmet', 'hafifle', 'havalan', 'hastalan', 'hahla', 'fethet', 'sahiplen'
-               , 'hapset', 'hareketlen', 'buharlah', 'hıçkır', 'hüphelen', 'mahvet', 'kamah', 'hırpala', 'hatırla'
-               , 'haberleh', 'heyecanlan']
-    h_correction = {'hohlan': 'hoşlan', 'hahla': 'haşla', 'buharlah': 'buharlaş', 'hüphelen': 'şüphelen',
-                    'kamah': 'kamaş', 'haberleh': 'haberleş'}
-    morph_correction = {'adlandır': 'adlan', 'bulundur': 'bulun', 'sınıflandır': 'sınıflan', 'görevlendir': 'görevlen',
-                        'haberleş': 'haberle', 'abart': 'abar', 'kararlaştır': 'kararla', 'sınırlandır': 'sınırlan',
-                        'ödüllendir': 'ödüllen', 'savrul': 'savrul', 'biçimlendir': 'biçimlen',
-                        'ilişkilendir': 'ilişkilen', 'isimlendir': 'isimlen', 'anlamlandır': 'anlamlan',
-                        'yardımlaş': 'yardımla'}
+    # For spelling and morphological correction
+    h_words = ['hazırla', 'hisset', 'hesapla', 'bahset', 'harca',
+               'hızlan', 'hedefle', 'rahatla', 'hohlan', 'hallet',
+               'zehirle', 'haykır', 'heyecan', 'hükmet', 'hafifle',
+               'havalan', 'hastalan', 'hahla', 'fethet', 'sahiplen',
+               'hapset', 'hareketlen', 'buharlah', 'hıçkır', 'hüphelen',
+               'mahvet', 'kamah', 'hırpala', 'hatırla', 'haberleh',
+               'heyecanlan']
 
-    stems_ = []
+    h_corrections = {'hohlan': 'hoşlan', 'hahla': 'haşla', 'buharlah': 'buharlaş',
+                     'hüphelen': 'şüphelen', 'kamah': 'kamaş', 'haberleh': 'haberleş'}
 
-    for s in stems:
-        s = s[1].split()[0]
-        s = s.replace('j', 'ğ')
-        if s not in h_words:
-            s = s.replace('h', 'ş')
-        elif s in h_correction:
-            s = h_correction[s]
+    morph_corrections = {'adlandır': 'adlan', 'bulundur': 'bulun', 'sınıflandır': 'sınıflan',
+                         'görevlendir': 'görevlen', 'haberleş': 'haberle', 'abart': 'abar',
+                         'kararlaştır': 'kararla', 'sınırlandır': 'sınırlan', 'ödüllendir': 'ödüllen',
+                         'savrul': 'savrul', 'biçimlendir': 'biçimlen', 'ilişkilendir': 'ilişkilen',
+                         'isimlendir': 'isimlen', 'anlamlandır': 'anlamlan', 'yardımlaş': 'yardımla'}
 
-        if s in morph_correction:
-            s = morph_correction[s]
+    # For storing corrected verb stems
+    corrected_stems = []
 
-        stems_.append(s)
+    # Apply corrections to verb stems
+    for index_stem in sorted(indices_stems):
+        corrected_stem = stem_correction(index_stem, h_words, h_corrections, morph_corrections)
+        corrected_stems.append(corrected_stem)
 
     # Save data
     with open('freq_dict_verbs.txt', 'w') as f:
-        f.write('\n'.join(stems_))
+        f.write('\n'.join(corrected_stems))
 
 
 if __name__ == '__main__':
