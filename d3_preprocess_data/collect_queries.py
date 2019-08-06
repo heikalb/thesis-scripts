1efacd0c69d2da845840c2ebbfc7f776833a6a84
hbadrulh <heikal93@gmail.com>
2019-08-06 16:46:39 -0700

    meep

--- d3_preprocess_data/collect_queries.py
+++ d3_preprocess_data/collect_queries.py
@@ -1,6 +1,7 @@
 # —*— coding: utf—8 —*—
 """
-Create one file or multiple separate files with target verbs and their context windows are joined.
+Create one file or multiple separate files with target verbs and their context
+windows are joined.
 Heikal Badrulhisham <heikal93@gmail.com>, 2019
 """
 import csv
@@ -8,13 +9,17 @@ import os
 import re
 from spelling_sub import suggestions
 
-sentence_punct = ['.', '!', '?']
 
-
-# Apply spelling correction based on spell correction suggestion file
 def apply_correction(target_word):
+    """
+    Apply spelling correction based on spell correction suggestion file.
+    :param target_word: word to be spell-corrected
+    :return: spell-corrected word
+    """
+    # Normalize the word
     target_word = target_word.lower()
 
+    # Apply spelling transformations
     for sug in suggestions:
         if re.search(sug, target_word):
             return re.sub(sug, suggestions[sug], target_word)
@@ -22,19 +27,32 @@ def apply_correction(target_word):
     return target_word
 
 
-# Given a sentence that may span multiple sentence boundaries, return the 
-# sentence containing the target word only and the index of the target word
 def to_one_sentence(sentence, target_i):
+    """
+    Given a sentence that may span multiple sentence boundaries, return the
+    sentence containing the target word only and the index of the target word.
+    :param sentence: the sentence
+    :param target_i: index of the target verb
+    :return: pair containing the reduced sentence and index of the target verb
+    """
+    # Tokenize the sentence
     tokens = sentence.split()
-    right_sent = []
-    left_sent = []
 
+    # Sub-sentence to the left and right of the target verb
+    left_sent = []
+    right_sent = []
+
+    # Sentence boundary characters
+    sentence_punct = ['.', '!', '?']
+
+    # Build the right sub-sentence
     for t in tokens[target_i:]:
         right_sent.append(t)
 
         if any(punct in t for punct in sentence_punct):
             break
 
+    # Build the left sub-sentence
     i = target_i - 1
 
     while i >= 0:
@@ -44,17 +62,25 @@ def to_one_sentence(sentence, target_i):
         left_sent.insert(0, tokens[i])
         i -= 1
 
+    # Join the entire sentence containing the target verb
     return ' '.join(left_sent + right_sent), len(left_sent)
 
 
-# Remove punctuation from a string
 def depunctuate(st):
+    """
+    Remove punctuation from a string.
+    :param st: the string
+    :return: depuntuated string
+    """
+    # Normalize the string
     st = st.strip().lower().split()
     new_sent = []
 
     for w in st:
+        # Remove non-letters/numbers
         new_word = [ch for ch in w if ch.isalnum()]
 
+        # Exclude purely punctuation words
         if new_word:
             new_sent.append(''.join(new_word))
 
@@ -62,8 +88,19 @@ def depunctuate(st):
 
 
 def fix_columns(left, mid, right, stem):
+    """
+    Fix errors related to sentence/word columns in the dataset.
+    :param left: the left column
+    :param mid: the middle word column
+    :param right: the right column
+    :param stem: the stem of the target verb
+    :return: the fixed columns
+    """
+    # Tokenize the middle column if there are multiple words in it
     mid_words = mid.split()
 
+    # If if there are multiple words in middle column, fix it fix occurrences
+    # of repeated words in the surrounding columns
     if len(mid_words) > 1:
         if left.endswith(mid):
             left = left.replace(mid, '').strip()
@@ -75,10 +112,16 @@ def fix_columns(left, mid, right, stem):
         else:
             mid = mid_words[0]
 
+    # Return fixed columns
     return left, mid, right
 
 
 def save_file(fname, data_list):
+    """
+    Save processed data.
+    :param fname: filename to save in
+    :param data_list: processed dataset
+    """
     with open(fname, 'w') as f:
         csv_writer = csv.writer(f, delimiter='\t')
 
@@ -87,6 +130,9 @@ def save_file(fname, data_list):
 
 
 def main():
+    """
+    Main method.
+    """
     save_rows = []
     data_dir = '../d2_data/query_results_freq_dict/'
 
@@ -124,8 +170,6 @@ def main():
             # Save data in individual files by verb
             file = f'../d2_data/joined/{filename[:3]}_{curr_stem}_joined.tsv'
             save_file(file, save_rows)
-            save_rows.clear()
-            rows.clear()
 
 
 if __name__ == "__main__":
