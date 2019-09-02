4da1defd94c13a4ca4cf0987a53e1dedf6a4fbde
hbadrulh <heikal93@gmail.com>
2019-09-01 22:34:42 -0700

    docs

--- d5_statistics/colloc_measures.py
+++ d5_statistics/colloc_measures.py
@@ -1,5 +1,5 @@
 """
-Implementation of various measures of collocation strength
+Implementation of various measures of association.
 Heikal Badrulhisham <heikal93@gmail.com>, 2019
 """
 
@@ -7,6 +7,13 @@ import math
 
 
 def get_freq(s1, s2, pairs):
+    """
+    Get the frequencies in a contingency table for a collocate pair.
+    :param s1: the first suffix
+    :param s2: the second suffix
+    :param pairs: the collocate pair
+    :return: frequencies in a contingency table
+    """
     a = max(sum(pairs[k] for k in pairs if s1 == k[0] and s2 == k[1]), 0.5)
     b = max(sum(pairs[k] for k in pairs if s1 == k[0] and s2 != k[1]), 0.5)
     c = max(sum(pairs[k] for k in pairs if s1 != k[0] and s2 == k[1]), 0.5)
@@ -16,6 +23,14 @@ def get_freq(s1, s2, pairs):
 
 
 def get_freq_(f_s1, f_s2, f_s1s2, total):
+    """
+    Get the frequencies in a contingency table for a collocate pair (alternative
+    method).
+    :param s1: the first suffix
+    :param s2: the second suffix
+    :param pairs: the collocate pair
+    :return: frequencies in a contingency table
+    """
     a = f_s1s2
     b = max(f_s1 - f_s1s2, 0.5)
     c = max(f_s2 - f_s1s2, 0.5)
@@ -25,6 +40,14 @@ def get_freq_(f_s1, f_s2, f_s1s2, total):
 
 
 def mutual_info(f_s1, f_s2, f_s1s2, total, *misc):
+    """
+    Get the mutual information of a collocate pair.
+    :param f_s1: frequency of the first suffix
+    :param f_s2: frequency of the second suffix
+    :param f_s1s2: frequency of the two suffixes together
+    :param total: total number of suffixes in the corpus
+    :return: mutual information of the collocate pair
+    """
     p_1 = float(f_s1) / float(total)
     p_2 = float(f_s2) / float(total)
     p_1_2 = float(f_s1s2) / float(total)
@@ -32,8 +55,15 @@ def mutual_info(f_s1, f_s2, f_s1s2, total, *misc):
     return math.log(p_1_2 / (p_1 * p_2), 2)
 
 
-# t-score
 def t_score(f_s1, f_s2, f_s1s2, total, *misc):
+    """
+    Get the t-score of a collocate pair.
+    :param f_s1: frequency of the first suffix
+    :param f_s2: frequency of the second suffix
+    :param f_s1s2: frequency of the two suffixes together
+    :param total: total number of suffixes in the corpus
+    :return: t-score of the collocate pair
+    """
     x_bar = float(f_s1s2) / float(total)
     p_1 = float(f_s1) / float(total)
     p_2 = float(f_s2) / float(total)
@@ -43,12 +73,29 @@ def t_score(f_s1, f_s2, f_s1s2, total, *misc):
     return (x_bar - mu)/math.sqrt(s_sq/total)
 
 
-# Dice coefficient
 def dice_coeff(f_s1, f_s2, f_s1s2, *misc):
+    """
+    Get the Dice coefficient of a collocate pair.
+    :param f_s1: frequency of the first suffix
+    :param f_s2: frequency of the second suffix
+    :param f_s1s2: frequency of the two suffixes together
+    :return: Dice coefficient of the collocate pair
+    """
     return 2*f_s1s2/(f_s1 + f_s2)
 
 
 def chi_sq(f_s1, f_s2, f_s1s2, total, s1, s2, pairs):
+    """
+    Get the chi-squared of a collocate pair.
+    :param f_s1: frequency of the first suffix
+    :param f_s2: frequency of the second suffix
+    :param f_s1s2: frequency of the two suffixes together
+    :param total: total number of suffixes in the corpus
+    :param s1: the first suffix
+    :param s2: the second suffix
+    :param pairs: the collocate pair
+    :return: chi-squared of the collocate pair
+    """
     f_u = f_s1
     f_0u = total - f_u
     f_v = f_s2
@@ -63,22 +110,53 @@ def chi_sq(f_s1, f_s2, f_s1s2, total, s1, s2, pairs):
 
 
 def risk_ratio(f_s1, f_s2, f_s1s2, total, s1, s2, pairs):
+    """
+    Get the risk ratio of a collocate pair.
+    :param f_s1: frequency of the first suffix
+    :param f_s2: frequency of the second suffix
+    :param f_s1s2: frequency of the two suffixes together
+    :param total: total number of suffixes in the corpus
+    :param s1: the first suffix
+    :param s2: the second suffix
+    :param pairs: the collocate pair
+    :return: risk ratio of the collocate pair
+    """
     a, b, c, d = get_freq(s1, s2, pairs)
     rr = (a / (a + b)) / (c / (c + d))
-    ci = rel_risk_ci(a, b, c, d, rr)
+    ci = risk_ratio_ci(a, b, c, d, rr)
 
     return rr, ci, (b == 0.5 or c == 0.5 or d == 0.5)
 
 
 def risk_ratio_reverse(f_s1, f_s2, f_s1s2, total, s1, s2, pairs):
+    """
+    Get the risk ratio reverse of a collocate pair.
+    :param f_s1: frequency of the first suffix
+    :param f_s2: frequency of the second suffix
+    :param f_s1s2: frequency of the two suffixes together
+    :param total: total number of suffixes in the corpus
+    :param s1: the first suffix
+    :param s2: the second suffix
+    :param pairs: the collocate pair
+    :return: risk ratio reverse of the collocate pair
+    """
     a, b, c, d = get_freq(s1, s2, pairs)
     rr = (a / (a + c)) / (b / (b + d))
-    ci = rel_risk_ci(a, b, c, d, rr)
+    ci = risk_ratio_ci(a, b, c, d, rr)
 
     return rr, ci
 
 
-def rel_risk_ci(a, b, c, d, rr):
+def risk_ratio_ci(a, b, c, d, rr):
+    """
+    Get the risk ratio of a confidence interval value.
+    :param a: the upper left cell of the contingency table
+    :param b: the lower left cell of the contingency table
+    :param c: the upper right cell of the contingency table
+    :param d: the lower right cell of the contingency table
+    :param rr: the risk ratio value
+    :return: lower and upper bounds of the confidence intervals
+    """
     stand_err = math.sqrt(1/a + 1/b + 1/c + 1/d)
     ci_r = math.log(rr) + 1.96*stand_err
     ci_l = math.log(rr) - 1.96*stand_err
@@ -87,6 +165,17 @@ def rel_risk_ci(a, b, c, d, rr):
 
 
 def odds_ratio(f_s1, f_s2, f_s1s2, total, s1, s2, pairs):
+    """
+    Get the odds-ratio of a collocate pair.
+    :param f_s1: frequency of the first suffix
+    :param f_s2: frequency of the second suffix
+    :param f_s1s2: frequency of the two suffixes together
+    :param total: total number of suffixes in the corpus
+    :param s1: the first suffix
+    :param s2: the second suffix
+    :param pairs: the collocate pair
+    :return: odds ratio of the collocate pair
+    """
     a, b, c, d = get_freq(s1, s2, pairs)
     condp_s2_s1 = a/(a + b)
     condp_s2_nots1 = c/(c + d)
