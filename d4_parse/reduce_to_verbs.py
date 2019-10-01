2388b0a447c0c7735556065210e0cebecc4536eb
hbadrulh <heikal93@gmail.com>
2019-10-01 11:46:37 -0700

    Amelioration

--- d4_parse/reduce_to_verbs.py
+++ d4_parse/reduce_to_verbs.py
@@ -8,53 +8,50 @@ import os
 from collections import Counter
 
 
-def get_indices(fpath):
+def get_indices(file_path):
     """
     Get position of target verbs in the context windows in a data file.
-    :param fpath: path of data file
+    :param file_path: path of data file
     :return: list of indices of target verbs
     """
 
-    with open(fpath) as f:
+    with open(file_path) as f:
         reader = csv.reader(f, delimiter='\t')
         indices = [int(row[2]) for row in reader]
 
     return indices
 
 
-def get_register(fpath):
+def get_register(file_path):
     """
     Get register of data points in a data file.
-    :param fpath: path of data file
+    :param file_path: path of data file
     :return: list of registers for data points ('w' or 'r')
     """
-    with open(fpath) as f:
+    with open(file_path) as f:
         reader = csv.reader(f, delimiter='\t')
         indices = [row[-1] for row in reader]
 
     return indices
 
 
-def main():
+def get_parses(parse_file_names, parse_dir):
     """
-    Main method.
+    Get verb parses and parsing errors.
+    :param parse_file_names: list of filenames of parse files
+    :param parse_dir: the directory of parse files
+    :return: a list of verb parses and a list of parsing errors
     """
-    # Get individual verb parse files
-    parse_dir = 'parses/'
-    parse_files = os.listdir(parse_dir)
-    parse_fnames = [f for f in parse_files if 'parses.txt' in f]
-    parse_fnames.sort()
-
     verb_parses = []
     parse_errors = []
 
-    for fname in parse_fnames:
-        print(fname)
-        parses = open(os.path.join(parse_dir, fname), 'r').read().split('\n')
+    # Process each parse file
+    for file_name in parse_file_names:
+        file_path = os.path.join(parse_dir, file_name)
+        parses = open(file_path, 'r').read().split('\n')
 
         # Get indices of target verbs
-        fnum = fname.split('_')[0]
-        stem = fname.split('_')[1]
+        fnum, stem = file_name.split('_')[:-1]
         indices = get_indices(f'../d2_data/joined/{fnum}_{stem}_joined.tsv')
         registers = get_register(f'../d2_data/joined/{fnum}_{stem}_joined.tsv')
 
@@ -63,16 +60,22 @@ def main():
             cur_parses = [p for p in parses[i][1:-1].split(', ')
                           if ':Punc' not in p]
 
+            # Divide data into verb parses and parsing errors
             if 'UNK' not in cur_parses[indices[i]]:
                 new_line = f'{cur_parses[indices[i]]} {registers[i]} {stem}'
                 verb_parses.append(new_line)
             else:
                 parse_errors.append(cur_parses[indices[i]])
 
-    print(len(verb_parses), len(verb_parses)-len(parse_errors))
-    print(len(parse_errors))
+    return verb_parses, parse_errors
 
-    # Save data and parse errors
+
+def save_data(verb_parses, parse_errors):
+    """
+    Save verb parses and parsing errors in .txt files.
+    :param verb_parses: list of verb parses
+    :param parse_errors: list of parsing errors
+    """
     with open('verb_parses.txt', 'w') as f:
         f.write('\n'.join(verb_parses))
 
@@ -83,7 +86,28 @@ def main():
     with open('parse_errors.txt', 'w') as f:
         lines = [f'{k} {parse_error_counter[k]}' for k in parse_errors]
         f.write('\n'.join(lines))
-        f.wr
+
+
+def main():
+    """
+    Main method.
+    """
+    # Get individual verb parse files
+    parse_dir = 'parses/'
+    parse_files = os.listdir(parse_dir)
+    parse_file_names = [f for f in parse_files if 'parses.txt' in f]
+    parse_file_names.sort()
+
+    # Get verb parses and parsing errors
+    verb_parses, parse_errors = get_parses(parse_file_names, parse_dir)
+
+    # Save data and parse errors
+    save_data(verb_parses, parse_errors)
+
+    # Display summary statistics
+    print('Number of verb parses: ', len(verb_parses))
+    print('Number of parse errors: ', len(parse_errors))
+    print('Net verb parses: ', len(parse_errors) - len(parse_errors))
 
 
 if __name__ == '__main__':
