04aad947bb22661f7440cb0b9282d10299731ebf
hbadrulh <heikal93@gmail.com>
2019-10-01 22:24:12 -0700

    Amelioration

--- d4_parse/src/ParseMorphemes.java
+++ d4_parse/src/ParseMorphemes.java
@@ -1,8 +1,12 @@
+/**
+ * Get morphological parses on TNC data.
+ * Heikal Badrulhisham <heikal93@gmail.com>, 2019
+ */
+
 import java.util.List;
 import zemberek.morphology.TurkishMorphology;
 import zemberek.morphology.analysis.SentenceAnalysis;
 import zemberek.morphology.analysis.WordAnalysis;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -12,45 +16,57 @@ import java.io.File;
 
 public class ParseMorphemes
 {
-    public static void main (String[] args) throws IOException
+    public static void main(String[] args) throws IOException
     {
-        // Corpus file
-        File[] query_files = new File("../d2_data/joined/").listFiles();
-        int i = 0;
-        for(File f: query_files)
+        /**
+         * Perform morphological parses on words in sentences in preprocessed
+         * TNC data file for all verb stems. Save parses in another set of files
+         * for all verb stems.
+         */
+        // Files of TNC query results by verbs
+        File[] queryFiles = new File("../d2_data/joined/").listFiles();
+
+        // Process each file
+        for(File f: queryFiles)
         {
+            // Read the content of the file
             String dataFile = f.getName();
             FileReader fileReader = new FileReader("../d2_data/joined/" + dataFile);
-            BufferedReader br = new BufferedReader(fileReader);
+            BufferedReader reader = new BufferedReader(fileReader);
 
-            // For Turkish morphological processing
-            TurkishMorphology morphology = TurkishMorphology.createWithDefaults();
+            // Turkish morphological parser
+            TurkishMorphology parser = TurkishMorphology.createWithDefaults();
 
-            //Collect parses
+            // For collecting parses
             ArrayList<List> parses = new ArrayList();
 
             // Go through lines in file
             String line;
-            while ((line = br.readLine()) != null)
+
+            while ((line = reader.readLine()) != null)
             {
+                // Get words in sentence as list
                 String sentence = line.split("\t")[0];
-                List<WordAnalysis> analyses = morphology.analyzeSentence(sentence);
-                SentenceAnalysis result = morphology.disambiguate(sentence, analyses);
-                parses.add(result.bestAnalysis());
+                // Get morphological parses for each word in sentence
+                List<WordAnalysis> analyses = parser.analyzeSentence(sentence);
+                // Disambiguate between multiple parses
+                SentenceAnalysis finalAnalysis = parser.disambiguate(sentence, analyses);
+                // Collect best parse
+                parses.add(finalAnalysis.bestAnalysis());
             }
 
             // Save data
-            String path = "parses/" + dataFile.split("_")[0] +"_" + dataFile.split("_")[1] + "_parses.txt";
+            String fileIndex = dataFile.split("_")[0];
+            String fileStem = dataFile.split("_")[1];
+            String path = "parses/" + fileIndex + "_" + fileStem + "_parses.txt";
             File saveFile = new File(path);
             FileWriter writer = new FileWriter(saveFile);
-            System.out.println(i + ": " + path);
 
             for(List parseList: parses)
                 writer.append(parseList.toString() + '\n');
 
             writer.flush();
             writer.close();
-            i++;
         }
     }
 }
